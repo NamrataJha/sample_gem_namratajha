ed9f182dad5d8859e6632b9c9966b6b896550a63
namratajha <namratajha@github.com>
2021-10-16 16:49:48 +0530

    gem

--- lib/sample_gem_namratajha.rb
+++ lib/sample_gem_namratajha.rb
@@ -5,4 +5,7 @@ require_relative "sample_gem_namratajha/version"
 module SampleGemNamratajha
   class Error < StandardError; end
   # Your code goes here...
+  def self.sample_gem_namratajha
+    puts "Hello World"
+  end
 end
