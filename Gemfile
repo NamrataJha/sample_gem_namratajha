1d7607321d5c7742b9e1bea0b836e19034039cfa
namratajha <namratajha@github.com>
2021-10-16 16:36:22 +0530

    gem init

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in sample_gem_namratajha.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/sample_gem_namratajha.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sample_gem_namratajha/version"
+
+module SampleGemNamratajha
+  class Error < StandardError; end
+  # Your code goes here...
+end
